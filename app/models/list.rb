c5c5f6b047e11f8455805ab4ae14e237d034d510
fatima ahmed <fatymah001@gmail.com>
2020-01-15 20:47:04 +0300

    Add association between Item and List models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ApplicationRecord
+  has_many :items
+  accepts_nested_attributes_for :items,
+                                reject_if: ->(attributes) { attributes['name'].blank? }, allow_destroy: true
 end
