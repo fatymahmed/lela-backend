e90fab82a57ee29d7e68f47642d1f142b0708f11
fatima ahmed <fatymah001@gmail.com>
2020-01-15 21:53:12 +0300

    Implement Item controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,57 @@
+class ItemsController < ApplicationController  
+  
+  def create
+    @item=Item.new(item_params)
+    if @item.save
+      items = Item.all
+      render json: items, except: %i[created_at updated_at]
+    else
+      render json: {
+        status: :fail
+      }
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(params)
+      render json: {
+        status: :success,
+        item: item
+      }
+    else
+      render json: {
+        status: :fail
+      }
+    end 
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    if item.destroyed?
+      render json: {
+        status: :success
+      }
+    else
+      render json: {
+        status: :fail
+      }
+    end
+  end
+
+  def index
+    items = Item.all
+    render json: items, except: %i[created_at updated_at]
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'items/create'
+  get 'items/show'
+  get 'items/update'
+  get 'items/destroy'
+  get 'items/index'
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
   delete :logout, to: 'sessions#logged_out'
