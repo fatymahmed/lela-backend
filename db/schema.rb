fc257c1b0dc2c6aa5a498fa5e6059f6eb8b0dd8a
fatima ahmed <fatymah001@gmail.com>
2020-01-15 20:44:52 +0300

    Add List model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_174132) do
+ActiveRecord::Schema.define(version: 2020_01_15_174428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_01_15_174132) do
     t.string "name"
     t.integer "price"
     t.string "category"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
